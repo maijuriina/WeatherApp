488761b1c5ac4c151bce0f150b79a3770792d5f5
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2021-01-25 13:07:03 +0200

    Initial commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Maijuriina Mustonen on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
